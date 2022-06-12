6d7300dcda3f7133322e1757ae901b8253e0a8b6
knsnyo <isc963@naver.com>
2022-06-12 16:25:01 +0900

    20220612 nodejs post

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(3000, () => {
   console.log("start!! express server");
 });
 
+
 app.get("/", (req, res) => {
 	//res.send("hi friend!");
 	// __dirname === ./
 	res.sendFile(__dirname + "/public/main.html");
+});
+
+app.post("/emailPost", (req, res) => {
+	console.log(req.body);
+	res.send(`Welcome: ${req.body.email}`);
 });
