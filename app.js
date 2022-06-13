755b4d1c70bd54d5a098f7f1596185418fef579a
knsnyo <isc963@naver.com>
2022-06-13 14:55:20 +0900

    test ejs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 
 app.listen(3000, () => {
   console.log("start!! express server");
@@ -12,12 +13,12 @@ app.listen(3000, () => {
 
 
 app.get("/", (req, res) => {
-	//res.send("hi friend!");
 	// __dirname === ./
 	res.sendFile(__dirname + "/public/main.html");
 });
 
 app.post("/emailPost", (req, res) => {
 	console.log(req.body);
-	res.send(`Welcome: ${req.body.email}`);
+	//res.send(`Welcome: ${req.body.email}`);
+	res.render("email.ejs", {"email": req.body.email});
 });
