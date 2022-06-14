2642257f48748262580d27069d7607a6b7ebf71b
knsnyo <isc963@naver.com>
2022-06-14 18:39:37 +0900

    20220614 connect mysql

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+	host: "localhost",
+	port: 3306,
+	user: "root",
+	password: "1234",
+	database: "member",
+});
+
+connection.connect();
 
 app.use(express.static("public"));
 app.use(bodyParser.json());
